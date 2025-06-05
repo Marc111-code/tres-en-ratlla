95bdd51fd5d1ad0390b3c32f203eac2ce5097aea
Marc111-code <solerm.marc@gmail.com>
2025-06-05 19:26:59 +0200

    Create funcions.py

---
+++ funcions.py
@@ -0,0 +1,23 @@
+ def startBoard():
+    taulell = [' ',' ',' ',' ',' ',' ',' ',' ',' ',]
+    return taulell
+
+
+def drawBoard(l,n):
+    if len(l) != 9:
+        print('Error! Board badly designed!')
+    else:
+        print("Turn number:", n)
+        print('='*12)
+        print(' '*3 + '|'+' '*3 + '|'+' '*3)  
+        print(' ' + l[6] + ' '+ '|' + ' ' + l[7]+' ' + '|'+ ' '+ l[8]+' ')
+        print(' '*3 + '|'+' '*3 + '|'+' '*3)  
+        print('-'*12)
+        print(' '*3 + '|'+' '*3 + '|'+' '*3)  
+        print(' ' + l[3] + ' '+ '|' + ' ' + l[4]+' ' + '|'+ ' '+ l[5]+' ')
+        print(' '*3 + '|'+' '*3 + '|'+' '*3)   
+        print('-'*12)
+        print(' '*3 + '|'+' '*3 + '|'+' '*3)  
+        print(' ' + l[0] + ' '+ '|' + ' ' + l[1]+' ' + '|'+ ' '+ l[2]+' ')
+        print(' '*3 + '|'+' '*3 + '|'+' '*3)  
+        print('='*12)
